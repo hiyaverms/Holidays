85d43cd2711e491430d0178cc061d6cf0d03a8fb
Hiya Verma <hiyaverm@bu.edu>
2026-03-26 23:28:06 -0400

    dependency issue

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ['@tailwindcss/oxide'],
 };
 
 export default nextConfig;
